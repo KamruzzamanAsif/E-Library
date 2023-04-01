eeb7046a4624443a03ed51128f6d3de8d8b1c6f1
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-04-02 00:19:19 +0600

    shared module added;

---
+++ e-library/src/app/shared/global-constant.ts
@@ -0,0 +1,16 @@
+export class GlobalConstants {
+    //message
+    public static genericError:string = 'Something went wrong! Try again later.';
+
+    // Regex
+    public static nameRegex:string = '^[a-zA-Z ]*$';
+    public static rollRegex:string = '^\d{4}$';
+    public static emailRegex:string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\$';
+    public static contactNumberRegex:string = '^\d{11}$';
+    public static sessionRegex:string = '^\d{2}-\d{2}$';
+    public static programLevelReges:string = '^[a-zA-Z ]{4}$';
+
+    // Variable 
+    public static error:string = 'error';
+
+}
