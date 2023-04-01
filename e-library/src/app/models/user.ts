cafda9001ee24a3cf3fe585314b42ab36a595e5d
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-04-02 00:21:06 +0600

    user class added

---
+++ e-library/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class user {
+
+    name: string = '';
+    roll: string = '';
+    batch: string = '';
+    session: string = '';
+    program_level: string = '';
+    mobile_number: string = '';
+    address: string = '';
+    email: string = '';
+    password: string = '';
+    
+}
